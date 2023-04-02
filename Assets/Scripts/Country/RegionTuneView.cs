d702b7fda0fc4ae9114ecfdbfd9d6bc6c4f6c218
Fromexx <pashaignat1003@gmail.com>
2023-04-02 17:01:24 +0500

    Write RegionTuneView

--- Assets/Scripts/Country/RegionTuneView.cs
+++ Assets/Scripts/Country/RegionTuneView.cs
@@ -9,31 +9,45 @@ namespace Country
     public class RegionTuneView : MonoBehaviour
     {
         [SerializeField] private GameObject _regionTune;
-        [SerializeField] private TMP_Text _tuneCountryBall;
-        [SerializeField] private TMP_Text _tuneMoney;
-        [SerializeField] private string _startTuneCountryBallText;
-        [SerializeField] private string _startTuneMoneyText;
+        [SerializeField] private TMP_Text _tuneCountryBallCost;
+        [SerializeField] private TMP_Text _tuneMoneyCost;
+        [SerializeField] private TMP_Text _currentCountryBallCount;
+        [SerializeField] private TMP_Text _currentMoney;
         
         private Region _region;
-        private bool _listenersAlreadyAdded;
 
         public void Render(TuneLevel tuneLevel, Region region)
         {
             _region = region;
             
             _regionTune.SetActive(true);
-            
-            if (tuneLevel.CurrentCountryBallLevel == tuneLevel.MaxLevel) _tuneCountryBall.text = _startTuneCountryBallText + tuneLevel.GetCountryBallTuneCount() + "\nMax Level";
-            else if (tuneLevel.CurrentCountryBallLevel != tuneLevel.MaxLevel) _tuneCountryBall.text = _startTuneCountryBallText + tuneLevel.GetCountryBallTuneCount();
 
-            if (tuneLevel.CurrentMoneyLevel == tuneLevel.MaxLevel) _tuneMoney.text = _startTuneMoneyText + tuneLevel.GetMoneyTune() + "%" + "\nMax Level";
-            else if (tuneLevel.CurrentMoneyLevel != tuneLevel.MaxLevel) _tuneMoney.text = _startTuneMoneyText + tuneLevel.GetMoneyTune() + "%";
+            if (tuneLevel.CurrentCountryBallLevel == tuneLevel.MaxLevel)
+            {
+                _tuneCountryBallCost.text = "-1";
+                _currentCountryBallCount.text = tuneLevel.GetCountryBallTuneCount().ToString();
+            }
+            else if (tuneLevel.CurrentCountryBallLevel != tuneLevel.MaxLevel)
+            {
+                _currentCountryBallCount.text = tuneLevel.GetCountryBallTuneCount().ToString();
+                _tuneCountryBallCost.text = tuneLevel.GetCountryBallTuneCost().ToString();
+            }
+
+            if (tuneLevel.CurrentMoneyLevel == tuneLevel.MaxLevel)
+            {
+                _tuneMoneyCost.text = "-1";
+                _currentMoney.text = tuneLevel.GetMoneyTune().ToString();
+            }
+            else if (tuneLevel.CurrentMoneyLevel != tuneLevel.MaxLevel)
+            {
+                _currentMoney.text = tuneLevel.GetMoneyTune().ToString();
+                _tuneMoneyCost.text = tuneLevel.GetMoneyTuneCost().ToString();
+            }
         }
 
         public void OnClose()
         {
             _regionTune.SetActive(false);
-            _listenersAlreadyAdded = false;
         }
 
         public void OnCountryBallTuneClick()
