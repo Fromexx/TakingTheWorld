0976d36e7600905013619e1cc5a6b1521c355d4d
aheront_aheront <k-maxim-k@mail.ru>
2023-04-07 19:40:23 +0700

    добавил камере края

--- Assets/Scripts/CameraLogic/CameraConstants.cs
+++ Assets/Scripts/CameraLogic/CameraConstants.cs
@@ -5,5 +5,11 @@ namespace Assets.Scripts.CameraLogic
     public static class CameraConstants
     {
         public static readonly Vector3 INITIAL_CAMERA_POSITION = new Vector3(883.1f, 231f, -92.4f);
+        
+        public static readonly float MAXIMUM_HORIZONTAL_POSITION = 1150;
+        public static readonly float MINIMUM_HORIZONTAL_POSITION = 330;
+
+        public static readonly float MAXIMUM_VERTICAL_POSITION = -60;
+        public static readonly float MINIMUM_VERTICAL_POSITION = -400;
     }
 }
--- Assets/Scripts/CameraLogic/PcCameraSwipes.cs
+++ Assets/Scripts/CameraLogic/PcCameraSwipes.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -10,11 +11,13 @@ namespace Assets.Scripts.CameraLogic
         private Vector3 _currentCameraPosition;
         private Vector3 _cameraPositionBeforeSwipe;
         private Vector3 _startSwipeMousePosition;
-        private float _sensitivity = 0.1f;
+        private float _sensitivity = 0.15f;
+        private Vector3 _cameraBorderCorrection;
 
         public PcCameraSwipes()
         {
             _cameraPositionBeforeSwipe = CameraConstants.INITIAL_CAMERA_POSITION;
+            _cameraBorderCorrection = Vector3.zero;
         }
         public Vector3 GetCameraPosition()
         {
@@ -23,22 +26,59 @@ namespace Assets.Scripts.CameraLogic
 
         public void UpdateCameraPosition()
         {
-                if (Input.GetMouseButtonDown((int)MouseButton.Left))
+            if (Input.GetMouseButtonDown((int)MouseButton.Left))
             {
                 _startSwipeMousePosition = GetCurrentMousePosition();
                 _cameraPositionBeforeSwipe = _currentCameraPosition;
+                _cameraBorderCorrection = Vector3.zero;
             }
 
             if (Input.GetMouseButtonUp((int)MouseButton.Left))
+            {
                 _cameraPositionBeforeSwipe = _cameraPositionBeforeSwipe + GetSwipeDelta();
+                _cameraBorderCorrection = Vector3.zero;
+            }
 
             if (Input.GetMouseButton((int)MouseButton.Left))
                 _currentCameraPosition = _cameraPositionBeforeSwipe + GetSwipeDelta();
             else
                 _currentCameraPosition = _cameraPositionBeforeSwipe;
+
+            PutCameraInBounds(
+                CameraConstants.MINIMUM_VERTICAL_POSITION,
+                CameraConstants.MAXIMUM_VERTICAL_POSITION,
+                ref _cameraBorderCorrection.z,
+                ref _currentCameraPosition.z
+                );
+
+            PutCameraInBounds(
+                CameraConstants.MINIMUM_HORIZONTAL_POSITION,
+                CameraConstants.MAXIMUM_HORIZONTAL_POSITION,
+                ref _cameraBorderCorrection.x,
+                ref _currentCameraPosition.x
+                );
         }
 
-        private Vector3 GetSwipeDelta() => (-1) *(GetCurrentMousePosition() - _startSwipeMousePosition) * _sensitivity;
+        private void PutCameraInBounds(float minimumCoord, float maximumCoord, ref float borderCorrection, ref float cameraPosition)
+        {
+            if (minimumCoord > maximumCoord)
+                throw new ArgumentException($"Минимальное положение камеры не может быть больше максимального\nМаксимальное: {maximumCoord}\nМинимальное: {minimumCoord}");
+
+            if(cameraPosition < minimumCoord)
+            {
+                borderCorrection += minimumCoord - cameraPosition;
+                cameraPosition = minimumCoord;
+            }
+
+            if(cameraPosition > maximumCoord)
+            {
+                borderCorrection += maximumCoord - cameraPosition;
+                cameraPosition = maximumCoord;
+            }
+        }
+
+        private Vector3 GetSwipeDelta() => 
+            (-1) *(GetCurrentMousePosition() - _startSwipeMousePosition) * _sensitivity + _cameraBorderCorrection;
 
         private Vector3 GetCurrentMousePosition()
         {
